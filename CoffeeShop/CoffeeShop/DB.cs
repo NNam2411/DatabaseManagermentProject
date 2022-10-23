419c96a5361e547d139fea114b3fc30289cb540a
NNam2411 <nguyennam2002ro@gmail.com>
2022-10-23 10:38:54 +0700

    Project

---
+++ CoffeeShop/CoffeeShop/DB.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CoffeeShop
+{
+    class DB
+    {
+        public SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-0CLMOJLH;Initial Catalog=CafeManager;Integrated Security=True");
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+        public void openConnection()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+
+}
